ddde33a0f46d6cfb09836db68d396707d297da41
bedew <sunhe1007@126.com>
2017-11-22 20:53:51 -0500

    add an exec util

--- utils/common.js
+++ utils/common.js
@@ -1,3 +1,6 @@
+const { execSync } = require('child_process');
+const path = require('path');
+
 /**
  * Notes:
  *      1. null         => undefined
@@ -29,7 +32,25 @@ function copyParams(params, over = {}) {
     return Object.assign({}, params, over);
 }
 
+/**
+ * only support JavaScript, put the script in `script`
+ * directory
+ * 
+ * @param {*} script 
+ * @param {*} options
+ */
+function execScript(script, { child = true } = {}) {
+    if (child) {
+        return execSync(`node ./${script}.js`, {
+            cwd: path.join(__dirname, '..', 'scripts'),
+            stdio: 'inherit'
+        });
+    } else {
+        require(`../scripts/${script}`);
+    }
+}
 
 exports.arrayWrap = arrayWrap;
 exports.apiBase = apiBase;
 exports.copyParams = copyParams;
+exports.execScript = execScript;
