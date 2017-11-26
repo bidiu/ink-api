66cb2a4d77b5a6a2fe2c25ffda080f6f3bfb91e6
bedew <sunhe1007@126.com>
2017-11-26 18:15:12 -0500

    added the verifying acc_token logic

--- middleware/auth/acc-tokens.js
+++ middleware/auth/acc-tokens.js
@@ -2,19 +2,36 @@ const authConfig = require('../../config/auth.config');
 const authUtils = require('../../utils/auth');
 const InkError = require('../../common/models/ink-errors');
 const Res = require('../../common/models/responses');
+const userService = require('../../services/users.v1');
+const toRegex = require('path-to-regexp');
 
 /**
- * verify payload of the access_token
+ * verify payload of the access_token, and add the auth info to `req`
+ * 
+ * @return {Promise<void>} resolving undefined
  */
-function verifyPayload(payload, req) {
+async function verifyPayload(payload, req) {
+    let requestedPath = req.baseUrl + req.path;
+    let endpoint = payload.endpoints.find(({ path }) => toRegex(path).test(requestedPath));
 
+    if (endpoint && endpoint.methods.includes(req.method)) {
+        let auth = { sub: payload.sub } // user id
+        if (authConfig.eagerLoading) {
+            auth.user = await userService.retrieveV2({ id: auth.sub });
+        }
+        req.auth = auth;
+    } else {
+        // client is not authorized for the requested path
+        throw new InkError.NoAuthorization();
+    }
 }
 
 module.exports = async function(req, res, next) {
     try {
         let accToken = req.cookies['access_token'];
         let payload = await authUtils.verifyToken(accToken, 'access_token', authConfig.publicKey);
-        verifyPayload(payload, req);
+        await verifyPayload(payload, req);
+        // authorization is all good
         next();
     } catch (err) {
         if (err instanceof InkError.NoAuthorization) {
