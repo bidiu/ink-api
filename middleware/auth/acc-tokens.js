689843e5dd48f67a55e2c33b45db35e148afa971
bedew <sunhe1007@126.com>
2017-11-26 15:40:56 -0500

    added a stub for verifying authorization in api v1

---
+++ middleware/auth/acc-tokens.js
@@ -0,0 +1,27 @@
+const authConfig = require('../../config/auth.config');
+const authUtils = require('../../utils/auth');
+const InkError = require('../../common/models/ink-errors');
+const Res = require('../../common/models/responses');
+
+/**
+ * verify payload of the access_token
+ */
+function verifyPayload(payload) {
+
+}
+
+module.exports = async function(req, res, next) {
+    try {
+        let accToken = req.cookies['access_token'];
+        let payload = await authUtils.verifyToken(accToken, 'access_token', authConfig.publicKey);
+        verifyPayload(payload, req);
+        next();
+    } catch (err) {
+        if (err instanceof InkError.NoAuthorization) {
+            let resPayload = new Res.Forbidden();
+            res.status(resPayload.status).json(resPayload);
+        } else {
+            next(err);
+        }
+    }
+}
--- routers/api/api.js
+++ routers/api/api.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const v1Router = require('./v1');
+const accKeeper = require('../../middleware/auth/acc-tokens');
 
 
 const apiRouter = express.Router();
+
+// gatekeeper (verify the access_token)
+apiRouter.use(accKeeper);
 
 // /api/v1
 apiRouter.use('/v1', v1Router);
